8c3f56869c851303c47b74ff3b94686df97eb39a
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-09-03 20:44:18 -0300

    atualização

--- src/projetodevmedia/main.java
+++ src/projetodevmedia/main.java
@@ -3,6 +3,6 @@ package projetodevmedia;
 public class main {
     public static void main (String args[]){
         System.out.println("Olá, mundo");
-        System.out.println("Seja bem vindo");
+
     }
 }
