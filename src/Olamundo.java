af7d23cf0b74ce1f0b263b7fb0679f509e488cbd
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-09-03 21:33:03 -0300

    Olá mundo em java.

---
+++ src/Olamundo.java
@@ -0,0 +1,6 @@
+public class Olamundo {
+    public static void sayHello(){
+        System.out.println("Olá mundo!.");
+        System.out.println("Bem vindo ao meu repositório do GITHUB");
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,6 @@
+public class Program {
+public static void main (String [] args){
+    Olamundo olamundo = new Olamundo ();
+    Olamundo.sayHello();
+    }
+}
