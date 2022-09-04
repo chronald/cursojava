7844afa5104a4c941125dce12ff151d9f9522ed3
Christian Ronald <110695050+chronald@users.noreply.github.com>
2022-09-03 21:19:46 -0300

    att

--- src/projetodevmedia/main.java
+++ src/projetodevmedia/main.java
@@ -2,7 +2,7 @@ package projetodevmedia;
 
 public class main {
     public static void main (String args[]){
-        System.out.println("Olá, mundo");
+        System.out.println("Testando IDE");
 
     }
 }
